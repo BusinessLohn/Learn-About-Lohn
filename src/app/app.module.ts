e608fddcc3550c5e1f35ea93c4c3206593a1e112
DanielLohn <lohn.business@gmail.com>
2024-06-18 22:14:01 -0500

    again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/components/header/header.component';
+// import "@fontsource/jetbrains-mono"; // Defaults to weight 400
+// import "@fontsource/jetbrains-mono/400.css"; // Specify weight
+// import "@fontsource/jetbrains-mono/400-italic.css"; // Specify weight and style
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
